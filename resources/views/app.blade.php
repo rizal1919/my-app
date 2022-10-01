25c68031567e42e134fb2a1a8f0dfcd967b63139
rizal1919 <rizalfathurr1999@gmail.com>
2022-10-01 12:00:20 +0700

    enhancement

---
+++ resources/views/app.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- CSS from bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+    <!-- my logo icon -->
+    <link rel="shortcut icon" type="image/x-icon" href="/img/my-black-logo.png" />
+
+    <!-- my css file -->
+    <link rel="stylesheet" href="/css/app.css">
+
+    <title>Rizal De Studio</title>
+</head>
+<body>
+
+    <div class="container mt-5 mb-3">
+        <div class="row col-sm-8">
+            <h1 id="headline">Hi Recruiters!</h1>
+            <h1 id="headline">Your worth candidate is here</h1>
+        </div>
+    </div>
+    <div class="container mb-5">
+        <div class="row col-sm-6">
+            <div class="card">
+                <div class="card-body">
+                    <p class="card-title fw-bold">Iam A Programmer</p>
+                    <p class="card-text">Exhausted to find a great one candidate? follow me.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container mb-3">
+        <div class="row g-3 mb-3">
+            <div class="col-sm-3">
+                <img src="/img/my-profile-pic.png" width="300px" alt="profile-pic-illustration">
+            </div>
+            <div class="col-sm-7">
+                <p style="margin-top: 50px; text-align:justify;">My fullname is Rizal Fathurrahman. People call me Rizal. Iam the first child and 23 years old. As a freshgraduate in 2022 I've spent a lot of times to explore my self. Organizations are space where I generate softskills such as <span class="badge text-bg-dark">team-work</span>, <span class="badge text-bg-dark">leadership</span>, <span class="badge text-bg-dark">time-management</span>, and <span class="badge text-bg-dark">adaptability</span>. I relize that These softkills are not genuine, but worth for me to have it. Passionate in programming especially web development has change my vision to a new version of me. <span class="badge text-bg-dark">Laravel</span> is the first framework of <span class="badge text-bg-dark">PHP</span> I learned to navigate my journey as a web programmer. Looking for someone who has a vision for innovation development for better people's life? <span class="fw-bold">You got one.</span></p>
+            </div>
+        </div>
+    </div>
+    
+    <!-- JavaScript from bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+</body>
+</html>
